b5cf0cd5b5ff236a3ef76070ed888b15b9f1d2e9
Malone-Campelo <malonycampelo1@gmail.com>
2024-06-09 10:50:19 -0300

    projeto concluido

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const botoes = document.querySelectorAll(".botoes .botao");
-    const personagens = document.querySelectorAll(".personagens .personagem");
+document.addEventListener('DOMContentLoaded', function() {
+    const botoes = document.querySelectorAll(".botao");
+    const personagens = document.querySelectorAll(".personagem");
 
     botoes.forEach((botao, index) => {
-        botao.addEventListener("click", function() {
-            botoes.forEach(b => b.classList.remove("selecionado"));
-            personagens.forEach(p => p.classList.remove("selecionado"));
+        botao.addEventListener("click", () => {
+            const botaoSelecionado = document.querySelector(".botao.selecionado");
+            if (botaoSelecionado) {
+                botaoSelecionado.classList.remove("selecionado");
+            }
+            
             botao.classList.add("selecionado");
+
+            const personagemSelecionado = document.querySelector(".personagem.selecionado");
+            if (personagemSelecionado) {
+                personagemSelecionado.classList.remove("selecionado");
+            }
+            
             personagens[index].classList.add("selecionado");
         });
     });
